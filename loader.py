df7012973b7608fa59835af92edb1f0cdbfa9dd8
GTSilver <gtsilver.work@gmail.com>
2023-06-25 19:34:10 +0500

    Task 020: config loader

--- loader.py
+++ loader.py
@@ -1,18 +1,30 @@
 import json
 import pandas
 
-# class config:
+
+class Config:
+    filename_separator = ''
+
+    def __init__(self, config_path: str):
+        def read_json_attr(config_path_: str) -> {}:
+            with open(config_path_, 'r') as f:
+                return json.load(f)
+
+        def read_class_attr() -> {}:
+            attr = {}
+            for k, v in self.__class__.__dict__.items():
+                if "__" in k:
+                    continue
+                attr[k] = v
+            return attr
+
+        def fill_class(json_attr_: {}, class_attr_: {}):
+            for k in class_attr_.keys():
+                setattr(self, str(k), json_attr_[k])
+
+        json_attr = read_json_attr(config_path)
+        class_attr = read_class_attr()
+        fill_class(json_attr, class_attr)
 
 
-def load_config() -> dict:
-    with open("config.json", 'r') as f:
-        return json.load(f)
-
-aawd = object()
-
-class aawd:
-    art = 0
-
-setattr(aawd, 'art', '6')
-
-print(aawd.art)
+Config('config.json')
