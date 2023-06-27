ce66c394fb97fe361a8820226ee37a31e64cbabe
GTSilver <gtsilver.work@gmail.com>
2023-06-27 23:09:20 +0500

    Task 030: complited

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 from loader import *
-from selector import *
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression
+from dataframe_collector import *
+
 
 cfg = Config('config.json')
 dataframe_list = load_dataframe_list(cfg)
-setting_columns_list = get_setting_columns(cfg, data)
-result_column_list = select_result_column("Result", cfg, data)
+result_dataframe = dataframe_concat(['Result'], dataframe_list)
 
-
-
-
-
-# data_for_pca = data[0].copy()
-# for i in range(1, len(data) - 1):
-#     data_for_pca = pd.concat([data_for_pca, data[i]])
-# data_for_pca = data_for_pca.reset_index(drop=True)
-#
-# X = data_for_pca.copy()
-# del X["Result"]
-# del X["Custom"]
-#
-# pca = PCA()
-#
-# y = data_for_pca["Result"]
-#
-# model = LinearRegression()
-# model.fit(X, y)
-#
-# importance = model.coef_
-#
-# pca.fit(X)
-#
-# print(X.columns)
+column_list = dataframe_list[0].columns.to_list()
+setting_columns = column_list[column_list.index(cfg.last_column_separator_name) + 1:]
+setting_dataframe = dataframe_concat(setting_columns, dataframe_list)
