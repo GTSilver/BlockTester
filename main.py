4a70fe260ea4644a26d267952ed697e34a15809a
GTSilver <gtsilver.work@gmail.com>
2023-06-27 19:25:00 +0500

    Task 030: -

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 from loader import *
+from selector import *
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
 
 cfg = Config('config.json')
-data = load_block(cfg)
+dataframe_list = load_dataframe_list(cfg)
+setting_columns_list = get_setting_columns(cfg, data)
+result_column_list = select_result_column("Result", cfg, data)
+
+
+
+
+
+# data_for_pca = data[0].copy()
+# for i in range(1, len(data) - 1):
+#     data_for_pca = pd.concat([data_for_pca, data[i]])
+# data_for_pca = data_for_pca.reset_index(drop=True)
+#
+# X = data_for_pca.copy()
+# del X["Result"]
+# del X["Custom"]
+#
+# pca = PCA()
+#
+# y = data_for_pca["Result"]
+#
+# model = LinearRegression()
+# model.fit(X, y)
+#
+# importance = model.coef_
+#
+# pca.fit(X)
+#
+# print(X.columns)
