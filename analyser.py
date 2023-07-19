35e5b9a70be2481ecb517e3bdced9da8a6663a4c
GTSilver <gtsilver.work@gmail.com>
2023-07-20 00:40:29 +0500

    New result

--- analyser.py
+++ analyser.py
@@ -5,18 +5,20 @@ from loader import *
 
 def create_report(dataframe: pd.DataFrame, top_size: int):
     df_analyse = dataframe.copy()
-    df_analyse['sum values'] = dataframe.apply(sum, axis=1)
-    df_analyse['zero & low count'] = dataframe.apply(lambda x: (x <= 0).sum(), axis=1)
-    df_analyse['low zero count'] = dataframe.apply(lambda x: (x < 0).sum(), axis=1)
+    df_analyse['sum values'] = dataframe.apply(sum, axis = 1)
+    df_analyse['zero count'] = dataframe.apply(lambda x: (x == 0).sum(), axis = 1)
+    df_analyse['up zero count'] = dataframe.apply(lambda x: (x > 0).sum(), axis = 1)
+    df_analyse['low zero count'] = dataframe.apply(lambda x: (x < 0).sum(), axis = 1)
     df_analyse = df_analyse.sort_values(['sum values'], ascending = False)
 
-    result_dict = {#'all df': dataframe.stack(),
-                   'sum values': df_analyse['sum values'],
-                   'zero & low count': df_analyse['zero & low count'],
-                   'low zero count:': df_analyse['low zero count'],
-                   'low zero is 0 sum': df_analyse[df_analyse['low zero count'] == 0]['sum values']}
+    df_low_zero_null = df_analyse[df_analyse['low zero count'] == 0]
 
-    print(len(result_dict['low zero is 0 sum']))
+    result_dict = {'sum values all': df_analyse['sum values'],
+                   'sum values': df_low_zero_null['sum values'],
+                   'zero count': df_low_zero_null['zero count'],
+                   'up zero count': df_low_zero_null['up zero count']}
+
+    print(df_low_zero_null.shape[0])
 
     for i in range(len(result_dict)):
         key = list(result_dict.keys())[i]
@@ -29,9 +31,9 @@ def create_report(dataframe: pd.DataFrame, top_size: int):
 
 def get_round_stats(series: pd.Series, digits: int, top_size: int) -> pd.Series:
     series = series[:top_size]
-    result = pd.Series({'quantile 0.25': series.quantile(0.25),
+    result = pd.Series({'quant. 0.25': series.quantile(0.25),
                         'mean': series.mean(),
-                        'quantile 0.75': series.quantile(0.75),
+                        'quant. 0.75': series.quantile(0.75),
                         'moda': np.mean(series.mode()),
                         'std': series.std()})
 
